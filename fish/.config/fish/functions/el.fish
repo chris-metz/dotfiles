f3aaa704743a49041c71ed407ab06a7f8eebed66
Chris <hello@chrismetz.xyz>
2025-02-01 11:11:27 +0100

    Update el.fish

--- fish/.config/fish/functions/el.fish
+++ fish/.config/fish/functions/el.fish
@@ -1,3 +1,3 @@
-function el --wraps='eza -lg --group-directories-first ' --description 'alias el=eza -lg --group-directories-first '
-  eza -lg --group-directories-first  $argv
+function el --wraps='eza -lg --icons --group-directories-first ' --description 'alias el=eza -lg --group-directories-first '
+  eza -lg --icons --group-directories-first  $argv
 end
