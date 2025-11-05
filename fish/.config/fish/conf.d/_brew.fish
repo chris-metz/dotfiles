e5aefb69cfc3fee5861b620532cc04866b5fe131
Chris <hello@chrismetz.xyz>
2025-11-05 06:31:10 +0000

    improve _brew.fish

--- fish/.config/fish/conf.d/_brew.fish
+++ fish/.config/fish/conf.d/_brew.fish
@@ -1,5 +1,13 @@
 # eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
+set brew_path_linux /home/linuxbrew/.linuxbrew/bin
+
+if test -d $brew_path_linux
+    if not contains $brew_path_linux $fish_user_paths
+        fish_add_path $brew_path_linux
+    end
+end
+
 if type -q brew
     eval (brew shellenv)
 end
