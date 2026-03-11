8a95ce39cb0bb437aed3d249e90ebb6df712dd37
Chris <hello@chrismetz.xyz>
2026-03-11 16:24:19 +0100

    fish

--- common/.config/fish/conf.d/00-noninteractive.fish
+++ common/.config/fish/conf.d/00-noninteractive.fish
@@ -1,3 +1,5 @@
-if not status is-interactive
-    exec bash -l
-end
+# Disabled: breaks plasmalogin wayland-session script
+# (fish -c invocations become interactive bash, losing the original command)
+# if not status is-interactive
+#     exec bash -l
+# end
